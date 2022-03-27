44c6d7a971cd8d5989d73079e0c50f90941a63c1
yugmani <yugmani@yahoo.com>
2022-03-27 15:32:10 -0700

    Cloning and merging, Object.assign

--- index.js
+++ index.js
@@ -65,3 +65,59 @@ let d = {
 // a, b, c and d are independent objects
 console.log(a == b); //false
 console.log(c == d); //false
+
+// Cloning and merging, Object.assign
+// *******************************************
+
+// copying an object variable creates one more reference to the same object.
+//  How do we get a duplicate of an object?
+// Copying by reference is good most of the time.
+// But if we really want duplicate of an object, then we need to create a new object and replicate the structure of the existing one by iterating over its properties and copying them on the primitive level.
+
+let student = {
+  name: 'Risbon',
+  age: 23,
+};
+
+let clone = {};
+
+// let's copy all student properties into it(clone)
+for (let key in student) {
+  clone[key] = student[key];
+}
+
+console.log(clone); //{name: "Risbon", age: 23}
+console.log(student == clone); //false
+
+//let's change some content in clone object
+clone.name = 'Shelly';
+
+console.log(student.name); // Risbon still?
+console.log(clone.name); // Shelly
+
+// cloning using `Object.assign` method.
+// ------------------------------------------
+
+// Syntax: Object.assign(dest, [src1, src2, src3...])
+// The first argument 'dest' is a target object.
+// Further arguments src1, ...., are source objects.
+// It copies the properties of all source objects src1, ..., srcN into the target dest.
+// The call returns dest.
+// we can use it to merge several objects into one:
+let worker = {
+  name: 'Jennifer',
+};
+
+let labor = {
+  isExperienced: true,
+};
+
+let plumber = {
+  isTrained: true,
+};
+
+// copies all properties from labor and plumber into worker
+Object.assign(worker, labor, plumber);
+
+console.log(worker);  //{name: "Jennifer", isExperienced: true, isTrained: true}
+
